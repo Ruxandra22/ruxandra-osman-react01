90bc0e2211461802906d753c514bd8df9db00c69
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-10-07 21:21:21 +0300

    course 7 oct exercises

--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -31,3 +31,61 @@ const person = {
     },
   ],
 };
+
+console.warn('Folosind obiectul person si reduce, afiseaza in consola un string care contine skillurile de pe ' +
+  'pozitiile pare ale arrayului, separate prin virgula');
+let message = '';
+message = person.skills.reduce((message, skill, index, skills) => {
+  if (index % 2 !== 0) {
+    return message;
+  }
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+
+console.warn(' In mod similar, afiseaza skillurile care NU incep cu j.');
+message = person.skills.reduce((message, skill, index) => {
+  if(skill.startsWith('j')) {
+    return message;
+  }
+
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+
+console.warn('Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."\n');
+message = person.friends.reduce((message, {name, surname}, index, friends) => {
+  const punctuation = index === friends.length - 1 ? '.' : ', ';
+
+  return `${message}${name} ${surname}${punctuation}`;
+}, 'Prietenii mei se numesc ');
+console.log(message);
+
+
+console.warn(' Folosind reduce, afiseaza numarul total de ani pe care il au persoanele din arrayul friends, doar ' +
+  'daca varsta este mai mare sau egala cu 30 de ani.');
+message = person.friends.reduce((sumYears, friend) => {
+  const { age } = friend;
+  return age >= 30 ? sumYears + age : sumYears;
+}, 0);
+console.log(message);
+
+
+console.warn(' doar skillurile care nu incep cu j');
+let filteredSkills = person.skills.reduce((filteredSkills, skill) => {
+  if (skill.startsWith('j')) {
+    return filteredSkills;
+  }
+
+  filteredSkills.push(skill);
+  return filteredSkills;
+}, []);
+console.log(filteredSkills);
+
+// another solution
+filteredSkills = person.skills.filter((skill) => {
+  return !skill.startsWith('j');
+});
+console.log(filteredSkills);
