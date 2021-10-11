cfdad1715d6af4b95b190dc9ab6d24e8ab2a653f
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-10-11 16:24:25 +0300

    [Homework 8 oct] ex4 - react components

--- react-components/ex04/app.js
+++ react-components/ex04/app.js
@@ -28,7 +28,7 @@ const person = {
 };
 
 
-console.warn('Folosind Object.entries() pe proprietatea skills, afiseaza abilitatile persoanei daca acestea sunt ' +
+console.warn('1. Folosind Object.entries() pe proprietatea skills, afiseaza abilitatile persoanei daca acestea sunt ' +
   'true. Folosind propozitii de forma: “person.name cunoaste: html.” ' +
   '“person.name cunoaste: javaScript.”');
 // ['html', true], ['css', false]...
@@ -39,7 +39,7 @@ Object.entries(person.skills).forEach(([skill, known]) => {
 });
 
 
-console.warn('Prin aceeasi metoda, afiseaza o lista inversata cu numele complet inversat al prietenilor. \n');
+console.warn('2. Prin aceeasi metoda, afiseaza o lista inversata cu numele complet inversat al prietenilor. \n');
 Object.entries(person.friends).reverse().forEach(([, friend]) => {
   const { name, surname } = friend;
 
@@ -47,7 +47,7 @@ Object.entries(person.friends).reverse().forEach(([, friend]) => {
 });
 
 
-console.warn('Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.entries()');
+console.warn('3. Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.entries()');
 let message = Object.entries(person.friends).reduce((message, [, friend], index, friends) => {
   const { name } = friend;
   const length = friends.length;
@@ -56,6 +56,49 @@ let message = Object.entries(person.friends).reduce((message, [, friend], index,
   return `${message}${name}${punctuation}`;
 }, 'Prietenii mei sunt ');
 console.log(message);
+
+
+console.warn('4. In mod similar afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze:' +
+  ' “Diferenta de varsta intre Larry si Dragos este de xxx ani.” etc…\n');
+Object.entries(person.friends).forEach(([, friend]) => {
+  const difference = Math.abs(friend.age - person.age);
+  console.log(`Diferenta de varsta dintre ${friend.name} si Dragos este de ${difference} ani.`);
+});
+
+
+console.warn('5. Folosind Object.entries() pe proprietatea skills, afiseaza toate abilitatile din obiectul skills.');
+Object.entries(person.skills).forEach(([skill, ]) => {
+  console.log(skill);
+});
+
+
+console.warn('6. Prin aceeasi metoda, afiseaza o lista cu numele complet al prietenilor. ');
+let friendsList = [];
+Object.entries(person.friends).forEach(([, friend]) => {
+  const fullName = `${friend.name} ${friend.surname}`;
+  friendsList.push(fullName);
+});
+console.log(friendsList);
+/** How would question no.6 be done using reduce??*/
+
+
+
+console.warn('7. Afiseaza propozitia: “Prietenii mei sunt Larry Larryson, Steven Stevenson si Carol Carolson.” ' +
+  'folosind Object.entries()');
+message = Object.entries(person.friends).reduce((message, [, friend], index, friends) => {
+  const punctuation = index === friends.length - 1 ? '.' : index === friends.length - 2 ? ' si ' : ', ';
+  return `${message}${friend.name} ${friend.surname}${punctuation}`;
+}, 'Prietenii mei sunt ');
+console.log(message);
+
+
+console.warn('8. In mod similar, afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze:' +
+  ' “Larry are xx ani. Steven are …”');
+Object.entries(person.friends).forEach(([, friend]) => {
+  const { name, age } = friend;
+  console.log(`${name} are ${age} ani.`);
+});
+
 
 
 
