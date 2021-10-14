a53a11a51ddd32c0ebcb1968a21946de92e88daf
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-10-14 22:10:22 +0300

    Course 14 oct Swapi Vehicles

---
+++ swapi-dealership/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <header className="navbar-dark fixed-top bg-dark border-bottom border-warning">
+        <nav className="container d-flex justify-content-between">
+          <h1 className="display-6 text-warning">
+            Swapi Vehicles
+          </h1>
+          {/*Search*/}
+        </nav>
+      </header>
+      <main className="container mb-4">
+        Content
+      </main>
+
+      <footer className="container mb-4">
+        Footer
+      </footer>
+    </>
+  );
+}
+
+export default App;
