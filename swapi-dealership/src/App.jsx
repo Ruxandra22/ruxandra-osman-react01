40012462575945bde461b58c0c408f9fcdbe1199
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-10-19 22:23:54 +0300

    Course 19 oct

--- swapi-dealership/src/App.jsx
+++ swapi-dealership/src/App.jsx
@@ -1,23 +1,34 @@
-function App() {
+import { useReducer } from 'react';
+import Screen from './components/Screen';
+import { AppContext, appState, appStateReducer } from './contexts/AppContext';
+import Search from './legacy/Search';
+
+const App = () => {
+  const [state, dispatch] = useReducer(appStateReducer, appState);
+  const contextValue = {
+    state: state,
+    dispatch: dispatch,
+  };
+
+  const { currentScreen } = state;
+
   return (
-    <>
+    <AppContext.Provider value={contextValue}>
       <header className="navbar-dark fixed-top bg-dark border-bottom border-warning">
         <nav className="container d-flex justify-content-between">
-          <h1 className="display-6 text-warning">
-            Swapi Vehicles
-          </h1>
-          {/*Search*/}
+          <h1 className="display-6 text-warning">Swapi Vehicles</h1>
+
+          <Search></Search>
         </nav>
       </header>
-      <main className="container mb-4">
-        Content
+
+      <main className="container mt-7 mb-4">
+        <Screen screen={currentScreen}></Screen>
       </main>
 
-      <footer className="container mb-4">
-        Footer
-      </footer>
-    </>
+      <footer className="container mb-4">Footer</footer>
+    </AppContext.Provider>
   );
-}
+};
 
 export default App;
---
+++ swapi-dealership/src/contexts/AppContext.jsx
@@ -0,0 +1,26 @@
+import {createContext} from "react";
+
+export const AppContext = createContext();
+
+export const appState = {
+  currentScreen: 'home',
+  selected: null,
+};
+
+export const appStateReducer = (appState, { type, payload }) => {
+  if (type === 'setScreen') {
+    // payload commits to be something like 'home' 'products' etc..
+    return {
+      ...appState,
+      currentScreen: payload,
+    }
+  }
+
+  if (type === 'setSelected') {
+    return {
+      ...appState,
+      selected: payload,
+    }
+  }
+  return appState;
+}
