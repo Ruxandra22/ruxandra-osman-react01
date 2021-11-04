8d92d601f8db344c3e66fda8fea093a19cd31746
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-11-04 18:25:40 +0200

    Course 2 Nov

---
+++ generic-game/src/App.jsx
@@ -0,0 +1,41 @@
+import { Footer, Header } from './components/common';
+import {useDispatch, useSelector} from "react-redux";
+import {clickClicker, decrementClicker} from "./actions/creators/ui";
+
+export const App = () => {
+  const clicker = useSelector((state => {
+    const { ui } = state;
+
+    return ui.clicker;
+  }));
+  const dispatch = useDispatch();
+
+
+  return (
+    <>
+      <Header/>
+      <main>
+        <div>Value is: {clicker}</div>
+        <button
+          onClick={() => {
+            dispatch(decrementClicker());
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            dispatch(clickClicker());
+          }}
+        >
+          Test
+        </button>
+      </main>
+      <Footer></Footer>
+    </>
+  )
+};
+
+export default App;
+// actions -> {type: '', payload: {}}  /types /creators
+// reducers
